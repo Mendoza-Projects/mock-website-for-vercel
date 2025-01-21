419192ce4752d5a0a11885fde6837e53c71e2d2e
Mendoza <gbenlemon@gmail.com>
2025-01-21 00:02:44 -0600

    changed header background

--- src/app/Header.tsx
+++ src/app/Header.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <header className="bg-blue-500 text-white p-4 text-center">
+    <header className="bg-red-500 text-white p-4 text-center">
       <h1 className="text-3xl">321 Mafia Beats</h1>
     </header>
   );
