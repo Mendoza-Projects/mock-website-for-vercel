57a109375e119cb39eeb416118749693880c8875
Mendoza <gbenlemon@gmail.com>
2025-01-20 23:15:53 -0600

    Add page and components for mock website

---
+++ src/app/Components/Body.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+const Body = () => {
+  return (
+    <main className="bg-black text-white p-6 flex items-center justify-center min-h-screen">
+      <h2 className="text-5xl font-bold">Hello World</h2>
+    </main>
+  );
+};
+
+export default Body;
