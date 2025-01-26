0cf04f79c34d4f533363bd6b2e2a730c3f68c013
Mendoza <gbenlemon@gmail.com>
2025-01-25 18:37:58 -0600

    fixed the link defined but not used

--- src/app/Body.tsx
+++ src/app/Body.tsx
@@ -6,7 +6,9 @@ const Body = () => {
   return (
     (<main className="flex-grow bg-black text-white p-8 text-center">
       <h2 className="text-6xl">Hello World</h2>
-      
+      <Link href="/beats" className="mt-4 text-lg text-blue-500">
+        Browse more beats
+      </Link>
     </main>)
   );
 };
