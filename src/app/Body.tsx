35414336a0865d6ad37cbd80501eff89e4a15a67
Mendoza <gbenlemon@gmail.com>
2025-01-25 18:33:54 -0600

    Linked beats in header to right link

--- src/app/Body.tsx
+++ src/app/Body.tsx
@@ -4,12 +4,10 @@ import Link from 'next/link';
 
 const Body = () => {
   return (
-    <main className="flex-grow bg-black text-white p-8 text-center">
+    (<main className="flex-grow bg-black text-white p-8 text-center">
       <h2 className="text-6xl">Hello World</h2>
-      <Link href="/beats">
-        <a className="mt-4 text-lg text-blue-500">Go to Beats</a>
-      </Link>
-    </main>
+      
+    </main>)
   );
 };
 
--- src/app/Header.tsx
+++ src/app/Header.tsx
@@ -4,25 +4,25 @@ import Link from 'next/link';
 
 const Header = () => {
   return (
-    <header className="bg-blue-500 text-white p-4">
+    (<header className="bg-blue-500 text-white p-4">
       <div className="flex justify-between items-center">
         <h1 className="text-3xl">321 Mafia Beats</h1>
         <nav>
           <ul className="flex space-x-4">
             <li>
-              <Link href="./">
+              <Link href="./" legacyBehavior>
                 <span className="hover:text-gray-300">Home</span>
               </Link>
             </li>
             <li>
-              <Link href="./test">
+              <Link href="/beats" legacyBehavior>
                 <span className="hover:text-gray-300">Beats</span>
               </Link>
             </li>
           </ul>
         </nav>
       </div>
-    </header>
+    </header>)
   );
 };
 
