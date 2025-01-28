f4495d24c7673b083d35a02c9757a885adf3e2d8
Mendoza <gbenlemon@gmail.com>
2025-01-28 00:57:51 -0600

    readded header bc while the page loaded, the login page failed

--- src/app/beats/page.tsx
+++ src/app/beats/page.tsx
@@ -1,7 +1,7 @@
 // src/app/beats/page.tsx
 'use client'
 
-
+import Header from "@/app/Header";
 import Footer from "@/app/Footer";
 import BeatPlayer from "../beatplayer";
 
@@ -10,7 +10,7 @@ import BeatPlayer from "../beatplayer";
 const BeatsPage = () => {
   return (
     <div className="min-h-screen flex flex-col">
-     
+      <Header />
       <div className="flex-1 bg-black text-white text-center py-20">
       <BeatPlayer />
       </div>
