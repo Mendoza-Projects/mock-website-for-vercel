73cdc8eeff9cb39bf546f54b200f4334df4af3d4
Mendoza <gbenlemon@gmail.com>
2025-01-28 02:30:15 -0600

    jdkglg

--- src/app/Footer.tsx
+++ src/app/Footer.tsx
@@ -1,9 +1,18 @@
-// src/app/Footer.tsx
-
 'use client'
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Footer = () => {
+  const [isClient, setIsClient] = useState(false); // Check if we're on the client-side
+
+  useEffect(() => {
+    // This effect runs only on the client side
+    setIsClient(true);
+  }, []);
+
+  if (!isClient) {
+    return null; // Render nothing if it's not on the client-side (SSR phase)
+  }
+
   return (
     <footer className="bg-gray-800 text-white p-4 text-center">
       <p>&copy; 2025 321 Mafia Beats. All Rights Reserved.</p>
