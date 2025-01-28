200ef97ca6c7cdcfb9ab64ddd8ed0eac89641fd7
Mendoza <gbenlemon@gmail.com>
2025-01-28 01:35:50 -0600

    still don't know what im doing lol

--- src/app/beats/page.tsx
+++ src/app/beats/page.tsx
@@ -1,22 +1,17 @@
-// src/app/beats/page.tsx
 'use client';
 
 import Header from "@/app/Header";
 import Footer from "@/app/Footer";
 import BeatPlayer from "../beatplayer";
 
-
-
-const BeatsPage = () => {
+export default function BeatsPage() {
   return (
     <div className="min-h-screen flex flex-col">
       <Header />
       <div className="flex-1 bg-black text-white text-center py-20">
-      <BeatPlayer />
+        <BeatPlayer />
       </div>
       <Footer />
     </div>
   );
-};
-
-
+}
