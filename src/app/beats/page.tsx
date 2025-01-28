28c368095fe5e74c85485cc393bec6b55e56e149
Mendoza <gbenlemon@gmail.com>
2025-01-28 02:34:01 -0600

    gjegkleg;ag

--- src/app/beats/page.tsx
+++ src/app/beats/page.tsx
@@ -1,11 +1,14 @@
 'use client';
 
 import React, { useEffect } from 'react';
-import Header from "@/app/Header";
-import Footer from "@/app/Footer";
-import BeatPlayer from "../beatplayer";
+import dynamic from 'next/dynamic';
 import { initializeApp } from 'firebase/app';
 import { firebaseConfig } from '@/app/firebase/config'; // Import the Firebase config
+
+// Dynamically import the components to avoid SSR issues
+const Header = dynamic(() => import('@/app/Header'), { ssr: false });
+const Footer = dynamic(() => import('@/app/Footer'), { ssr: false });
+const BeatPlayer = dynamic(() => import('../beatplayer'), { ssr: false });
 
 export default function BeatsPage() {
   useEffect(() => {
