3b8ee1d7cb4067ed7fb844ebb8d0f4d082423799
Mendoza <gbenlemon@gmail.com>
2025-01-28 01:40:17 -0600

    hopefully this time

--- src/app/beats/page.tsx
+++ src/app/beats/page.tsx
@@ -1,17 +1,27 @@
+// src/app/beats/page.tsx
 'use client';
 
+import React, { useEffect, useState } from 'react';
 import Header from "@/app/Header";
 import Footer from "@/app/Footer";
 import BeatPlayer from "../beatplayer";
 
-export default function BeatsPage() {
+const BeatsPage = () => {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);  // This will run once on the client after the component mounts
+  }, []);
+
   return (
     <div className="min-h-screen flex flex-col">
       <Header />
       <div className="flex-1 bg-black text-white text-center py-20">
-        <BeatPlayer />
+        {isClient ? <BeatPlayer /> : null}
       </div>
       <Footer />
     </div>
   );
-}
+};
+
+export default BeatsPage;
